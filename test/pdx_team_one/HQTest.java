9663e447b4c45a802582f74abbe0bd56adca650b
Lauren Braun <lbraun21@gmail.com>
2020-01-28 13:21:47 -0800

    add some testing framework

---
+++ test/pdx_team_one/HQTest.java
@@ -0,0 +1,27 @@
+package pdx_team_one;
+
+import org.junit.Test;
+
+public class HQTest {
+
+    @Test
+    void takeTurnShouldSetLocationOnFirstCall() {
+
+    }
+
+    @Test
+    void takeTurnShouldSendMessageIfInDanger() {
+
+    }
+
+    @Test
+    void takeTurnShouldBuildMiners() {
+
+    }
+
+    @Test
+    void takeTurnShouldShootWithEnemiesInRange() {
+
+    }
+
+}
---
+++ test/pdx_team_one/MinerTest.java
@@ -0,0 +1,21 @@
+package pdx_team_one;
+
+import battlecode.common.RobotController;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class MinerTest {
+    RobotController rc;
+    Miner testRobot = new Miner(rc);
+
+    @Test
+    void takeTurnWithNoSoupInRange() {
+
+    }
+
+    @Test
+    void takeTurnWithSoupInRange() {
+
+    }
+}
